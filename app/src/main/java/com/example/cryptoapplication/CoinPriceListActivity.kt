74f65fd7158f803abd13aafb89332dc970926265
Qarapapax <aldunsar@yahoo.com>
2023-02-11 20:23:29 +0300

    Layouts created

--- app/src/main/java/com/example/cryptoapplication/CoinPriceListActivity.kt
+++ app/src/main/java/com/example/cryptoapplication/CoinPriceListActivity.kt
@@ -4,23 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.ViewModelProvider
-import com.example.cryptoapplication.api.ApiFactory
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
-import io.reactivex.rxjava3.disposables.CompositeDisposable
-import io.reactivex.rxjava3.schedulers.Schedulers
 
-class MainActivity : AppCompatActivity() {
+class CoinPriceListActivity : AppCompatActivity() {
 
     private lateinit var coinViewModel: CoinViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_coin_price_list)
         coinViewModel = ViewModelProvider(this)[CoinViewModel::class.java]
 //        coinViewModel.priceList.observe(this) {
 //            Log.d("TEST_OF_LOADING_DATA", "Success ACTIVITY $it")
 //        }
-        coinViewModel.getDetailInfo("ETH").observe(this) {
+        coinViewModel.getDetailInfo("ETH").observe(this){
             Log.d("TEST_OF_LOADING_DATA", "Success ACTIVITY $it")
         }
     }
