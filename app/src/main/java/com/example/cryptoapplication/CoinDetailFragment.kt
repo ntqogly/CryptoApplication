f8341950e243fd938c520f58511bdefbfabc9ca5
Qarapapax <aldunsar@yahoo.com>
2023-02-12 12:50:07 +0300

    PriceListFragment only is ready

---
+++ app/src/main/java/com/example/cryptoapplication/CoinDetailFragment.kt
@@ -0,0 +1,69 @@
+package com.example.cryptoapplication
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.cryptoapplication.databinding.FragmentCoinDetailBinding
+import com.squareup.picasso.Picasso
+
+class CoinDetailFragment : Fragment() {
+
+    private lateinit var viewModel: ViewModel
+
+    private var _binding: FragmentCoinDetailBinding? = null
+    private val binding: FragmentCoinDetailBinding
+        get() = _binding ?: throw RuntimeException("FragmentCoinDetailBinding==null")
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentCoinDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if (!requireActivity().intent.hasExtra(EXTRA_FROM_SYMBOL)) {
+            requireActivity().finish()
+            return
+        }
+        val fromSymbol = requireActivity().intent.getStringExtra(EXTRA_FROM_SYMBOL)
+        viewModel = ViewModelProvider(this)[ViewModel::class.java]
+        if (fromSymbol != null) {
+            viewModel.getDetailInfo(fromSymbol).observe(viewLifecycleOwner) {
+                with(binding) {
+                    tvPrice.text = it.price
+                    tvMinPrice.text = it.lowDay
+                    tvMaxPrice.text = it.highDay
+                    tvLastMarket.text = it.lastMarket
+                    tvLastUpdate.text = it.getFormattedTime()
+                    tvFromSymbol.text = it.fromSymbol
+                    tvToSymbol.text = it.toSymbol
+                    Picasso.get().load(it.getFullImageUrl()).into(ivLogoCoin)
+                }
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+        private const val EXTRA_FROM_SYMBOL = "fSym"
+
+        fun newInstance(fromSymbol: String): CoinDetailFragment {
+            return CoinDetailFragment().apply {
+                arguments = Bundle().apply {
+                    putString(EXTRA_FROM_SYMBOL, fromSymbol)
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/cryptoapplication/CoinPriceListFragment.kt
@@ -0,0 +1,50 @@
+package com.example.cryptoapplication
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
+import com.example.cryptoapplication.adapter.CoinInfoAdapter
+import com.example.cryptoapplication.databinding.FragmentCoinPriceListBinding
+import com.example.cryptoapplication.pojo.CoinPriceInfo
+
+class CoinPriceListFragment : Fragment() {
+    private lateinit var viewModel: ViewModel
+    private lateinit var rvCoinPriceList: RecyclerView
+    private lateinit var binding: FragmentCoinPriceListBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentCoinPriceListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        rvCoinPriceList = binding.rvCoinPriceList
+        val coinInfoAdapter = CoinInfoAdapter(requireContext())
+        rvCoinPriceList.adapter = coinInfoAdapter
+        viewModel = ViewModelProvider(this)[ViewModel::class.java]
+        viewModel.priceList.observe(viewLifecycleOwner) {
+            coinInfoAdapter.coinInfoList = it
+        }
+
+        coinInfoAdapter.onCoinClickListener = object : CoinInfoAdapter.OnCoinClickListener {
+            override fun onCoinClick(coinPriceInfo: CoinPriceInfo) {
+                requireActivity().supportFragmentManager.beginTransaction()
+                    .replace(
+                        R.id.container,
+                        CoinDetailFragment.newInstance(coinPriceInfo.fromSymbol)
+                    )
+                    .addToBackStack(null)
+                    .commit()
+            }
+        }
+    }
+}
