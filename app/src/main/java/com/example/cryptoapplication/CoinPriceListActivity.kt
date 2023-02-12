bd877e938d07dd9b540c4eaee558896eff52a767
Qarapapax <aldunsar@yahoo.com>
2023-02-12 12:00:41 +0300

    Adapter, ViewBinding, First activity is ready

--- app/src/main/java/com/example/cryptoapplication/CoinPriceListActivity.kt
+++ app/src/main/java/com/example/cryptoapplication/CoinPriceListActivity.kt
@@ -4,20 +4,31 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
+import com.example.cryptoapplication.adapter.CoinInfoAdapter
+import com.example.cryptoapplication.pojo.CoinPriceInfo
 
 class CoinPriceListActivity : AppCompatActivity() {
 
+
     private lateinit var coinViewModel: CoinViewModel
+    private lateinit var rvCoinPriceList: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coin_price_list)
+        rvCoinPriceList = findViewById(R.id.rvCoinPriceList)
+        val coinInfoAdapter = CoinInfoAdapter(this)
+        coinInfoAdapter.onCoinClickListener = object : CoinInfoAdapter.OnCoinClickListener {
+            override fun onCoinClick(coinPriceInfo: CoinPriceInfo) {
+                Log.d("TEST", coinPriceInfo.fromSymbol)
+            }
+        }
+
+        rvCoinPriceList.adapter = coinInfoAdapter
         coinViewModel = ViewModelProvider(this)[CoinViewModel::class.java]
-//        coinViewModel.priceList.observe(this) {
-//            Log.d("TEST_OF_LOADING_DATA", "Success ACTIVITY $it")
-//        }
-        coinViewModel.getDetailInfo("ETH").observe(this){
-            Log.d("TEST_OF_LOADING_DATA", "Success ACTIVITY $it")
+        coinViewModel.priceList.observe(this) {
+            coinInfoAdapter.coinInfoList = it
         }
     }
 }
