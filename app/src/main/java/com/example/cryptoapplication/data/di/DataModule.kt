7694c680d8be542d9a2dec42ecf7829a044ff86f
Qarapapax <aldunsar@yahoo.com>
2023-02-17 16:41:58 +0300

    inject onto worker factory constructor

--- app/src/main/java/com/example/cryptoapplication/data/di/DataModule.kt
+++ app/src/main/java/com/example/cryptoapplication/data/di/DataModule.kt
@@ -3,6 +3,8 @@ package com.example.cryptoapplication.data.di
 import android.app.Application
 import com.example.cryptoapplication.data.database.AppDatabase
 import com.example.cryptoapplication.data.database.CoinInfoDao
+import com.example.cryptoapplication.data.network.ApiFactory
+import com.example.cryptoapplication.data.network.ApiService
 import com.example.cryptoapplication.data.repository.CoinRepositoryImpl
 import com.example.cryptoapplication.domain.CoinRepository
 import dagger.Binds
@@ -13,14 +15,22 @@ import dagger.Provides
 interface DataModule {
 
     @Binds
+    @ApplicationScope
     fun bindCoinRepository(impl: CoinRepositoryImpl): CoinRepository
 
     companion object {
         @Provides
+        @ApplicationScope
         fun provideCoinInfoDao(
             application: Application
         ): CoinInfoDao {
             return AppDatabase.getInstance(application).coinPriceInfoDao()
+        }
+
+        @Provides
+        @ApplicationScope
+        fun provideApiService(): ApiService {
+            return ApiFactory.apiService
         }
     }
 }
--- app/src/main/java/com/example/cryptoapplication/presentation/CoinApp.kt
+++ app/src/main/java/com/example/cryptoapplication/presentation/CoinApp.kt
@@ -2,24 +2,25 @@ package com.example.cryptoapplication.presentation
 
 import android.app.Application
 import androidx.work.Configuration
-import com.example.cryptoapplication.data.database.AppDatabase
 import com.example.cryptoapplication.data.di.DaggerApplicationComponent
-import com.example.cryptoapplication.data.network.ApiFactory
 import com.example.cryptoapplication.data.workers.RefreshDataWorkerFactory
+import javax.inject.Inject
 
 class CoinApp : Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory: RefreshDataWorkerFactory
 
     val component by lazy {
         DaggerApplicationComponent.factory().create(this)
     }
 
+    override fun onCreate() {
+        component.inject(this)
+        super.onCreate()
+    }
+
     override fun getWorkManagerConfiguration(): Configuration {
-        return Configuration.Builder().setWorkerFactory(
-            RefreshDataWorkerFactory(
-                AppDatabase.getInstance(this).coinPriceInfoDao(),
-                ApiFactory.apiService,
-                com.example.cryptoapplication.data.mapper.CoinMapper()
-            )
-        ).build()
+        return Configuration.Builder().setWorkerFactory(workerFactory).build()
     }
 }
